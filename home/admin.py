ca48e35a264dccf101fa2d363376f19d69419f85
peterhriser <peterhriser@gmail.com>
2017-02-01 23:47:54 +0000

    added functioning admin support, can now expand textbooks and pages in one entry

--- home/admin.py
+++ home/admin.py
@@ -2,6 +2,20 @@ from django.contrib import admin
 from home.models import *
 
 # Register your models here.
-admin.site.register(Page)
-admin.site.register(Textbook)
-admin.site.register(Section)
+
+from nested_inline.admin import NestedStackedInline, NestedModelAdmin
+
+class SectionInline(NestedStackedInline):
+    model = Section
+    extra = 1
+
+
+class PageInline(NestedStackedInline):
+    model = Page
+    extra = 1
+    inlines = [SectionInline]
+
+
+@admin.register(Textbook)
+class TextbookAdmin(NestedModelAdmin):
+    inlines = [PageInline]
